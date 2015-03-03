32df9fa8fe0ff4d249eaf9c6086de58e6648d4a7
Sam Pfeiffer <sampfeiffer54@gmail.com>
2015-03-03 18:41:12 -0500

    test12

--- AssignmentB/test_cva.cu
+++ AssignmentB/test_cva.cu
@@ -9,9 +9,13 @@ int main(void) {
     cudaGetDeviceProperties(&deviceProp1, 0);
     cudaGetDeviceProperties(&deviceProp2, 1);
 
-    printf("\nDevice 0 has %f MB of global RAM, while Device 1 has %f. Cheers!",
+    printf("\nDevice 0 has %f MB of global RAM, while Device 1 has %f. Cheers!\n",
             deviceProp1.totalGlobalMem / (1024. * 1024.),
             deviceProp2.totalGlobalMem / (1024. * 1024.));
+
+    int num_gpus=0;
+    cudaGetDeviceCount(&num_gpus);
+    printf("number of CUDA devices:\t%d\n", num_gpus);
 
     return 0;
 }
