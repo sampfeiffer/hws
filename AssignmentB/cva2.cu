6d333876002f8a65d9643d1220263a9f71fbcaa9
Sam Pfeiffer <sampfeiffer54@gmail.com>
2015-03-06 21:23:45 -0500

    test34

--- AssignmentB/cva2.cu
+++ AssignmentB/cva2.cu
@@ -84,6 +84,21 @@ int main(int argc, char *argv[])
         }
     }
 
+    std::vector<Swap> swap_vector_temp;
+    for (int k=0; k<params.swap_num/params.deals_at_once; ++k){
+        // Get swap deal data
+        swap_vector_temp.clear();
+        data.get_next_data_swap(swap_vector_temp, params);
+        thrust::device_vector<Swap> swap_vector(swap_vector_temp.begin(), swap_vector_temp.end());
+        thrust::device_vector<float> cva_vector(swap_vector.size());
+        thrust::transform(swap_vector.begin(), swap_vector.end(), cva_vector.begin(), calculate_cva_swap(params, num_of_steps));
+        thrust::host_vector<float> cva_vector_host(cva_vector);
+
+        for (unsigned int i=0; i<cva_vector_host.size(); ++i){
+            std::cout << "cva " << k*params.deals_at_once+i+1 << " " << cva_vector_host[i] << " " << swap_vector_temp[i].swap_id << "\n";
+        }
+    }
+
     //multiply by recovery
 
     data.close_files();
