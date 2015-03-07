8a95cbc231f98ae3a144432b6ab169f2e6652a45
Sam Pfeiffer <sampfeiffer54@gmail.com>
2015-03-06 21:20:39 -0500

    test33

--- AssignmentB/cva2.cu
+++ AssignmentB/cva2.cu
@@ -1,3 +1,8 @@
+//stop when you reach end of fx and switch to swap
+//turn cva for each deal into cva for each counterparty
+//multiple gpus
+//path creation off of device. pass to device
+
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 
@@ -8,11 +13,11 @@
 #include "state.h"
 #include "data_reader2.h"
 
-struct calculate_cva{
+struct calculate_cva_fx{
     Parameters params;
     int num_of_steps;
 
-    calculate_cva(Parameters params_, int num_of_steps_) : params(params_), num_of_steps(num_of_steps_)
+    calculate_cva_fx(Parameters params_, int num_of_steps_) : params(params_), num_of_steps(num_of_steps_)
     {}
     __device__ __host__
     float operator()(Fx &fx) {
@@ -22,7 +27,28 @@ struct calculate_cva{
         for (int i=0; i<num_of_steps; ++i){
             world_state.sim_next_step();
             prob_default = std::exp(-fx.hazard_rate*(world_state.time-1)/float(360.0)) - std::exp(-fx.hazard_rate*world_state.time/float(360.0));
-            cva += world_state.cva_disc_factor * prob_default * max(fx.value(world_state.fx_rate),float(0.0));;
+            cva += world_state.cva_disc_factor * prob_default * max(fx.value(world_state.fx_rate),float(0.0));
+        }
+        cva *= 1-params.recovery_rate;
+        return cva;
+    }
+};
+
+struct calculate_cva_swap{
+    Parameters params;
+    int num_of_steps;
+
+    calculate_cva_swap(Parameters params_, int num_of_steps_) : params(params_), num_of_steps(num_of_steps_)
+    {}
+    __device__ __host__
+    float operator()(Swap &sw) {
+        float cva=0;
+        float prob_default;
+        State world_state(params);
+        for (int i=0; i<num_of_steps; ++i){
+            world_state.sim_next_step();
+            prob_default = std::exp(-sw.hazard_rate*(world_state.time-1)/float(360.0)) - std::exp(-sw.hazard_rate*world_state.time/float(360.0));
+            cva += world_state.cva_disc_factor * prob_default * max(sw.value(world_state),float(0.0));
         }
         cva *= 1-params.recovery_rate;
         return cva;
@@ -39,32 +65,30 @@ int main(int argc, char *argv[])
 
     // Get parameters and initial state of the world.
     Parameters params(parameters_filename, state0_filename);
-    //params.print();
-
-    // Get counterparty data and store info in cp_vector_temp
-    std::vector<Fx> fx_vector_temp;
-    Data_reader data;
-    data.get_next_data_fx(fx_vector_temp, params);
-
-    thrust::device_vector<Fx> fx_vector(fx_vector_temp.begin(), fx_vector_temp.end());
-
     int num_of_steps = params.days_in_year*params.time_horizon/params.step_size;
 
-    end_time = clock() - program_start_time;
-    std::cout << "Timing: whole program " << float(end_time)/CLOCKS_PER_SEC << " seconds.\n";
 
-    thrust::device_vector<float> cva_vector(fx_vector.size());
-    //std::cout << "here1 " << cva_vector.size() << "\n";
-    thrust::transform(fx_vector.begin(), fx_vector.end(), cva_vector.begin(), calculate_cva(params, num_of_steps));
-    //std::cout << "here2\n";
-    thrust::host_vector<float> cva_vector_host(cva_vector);
+    Data_reader data;
+    std::vector<Fx> fx_vector_temp;
+    for (int k=0; k<params.fx_num/params.deals_at_once; ++k){
+        // Get fx deal data
+        fx_vector_temp.clear();
+        data.get_next_data_fx(fx_vector_temp, params);
+        thrust::device_vector<Fx> fx_vector(fx_vector_temp.begin(), fx_vector_temp.end());
+        thrust::device_vector<float> cva_vector(fx_vector.size());
+        thrust::transform(fx_vector.begin(), fx_vector.end(), cva_vector.begin(), calculate_cva_fx(params, num_of_steps));
+        thrust::host_vector<float> cva_vector_host(cva_vector);
 
-    for (unsigned int i=0; i<cva_vector_host.size(); ++i){
-        //std::cout << "here in " << i+1 << "\n";
-        std::cout << "cva " << i+1 << " " << cva_vector_host[i] << "\n";
+        for (unsigned int i=0; i<cva_vector_host.size(); ++i){
+            std::cout << "cva " << k*params.deals_at_once+i+1 << " " << cva_vector_host[i] << " " << fx_vector_temp[i].fx_id << "\n";
+        }
     }
 
+    //multiply by recovery
+
     data.close_files();
+    end_time = clock() - program_start_time;
+    std::cout << "Timing: whole program " << float(end_time)/CLOCKS_PER_SEC << " seconds.\n";
     std::cout << "\n";
     return 0;
 }
