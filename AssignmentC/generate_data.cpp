66b55df6bd1c2b7ca6e3ac1107e9682fb7c6e698
Sam Pfeiffer <sampfeiffer54@gmail.com>
2015-03-12 00:25:52 -0400

    start assignment C, generate the tick data

---
+++ AssignmentC/generate_data.cpp
@@ -0,0 +1,67 @@
+#include <random>
+#include <cmath>
+#include <time.h>
+#include "parameters.h"
+
+//change Average time between ticks in milliseconds to 9 to make 45GB
+
+int main(int argc, char *argv[])
+{
+    const char* parameters_filename = "parameters.txt";
+    const char* input_data_filename = "input_data.txt";
+    const char* tick_data_filename = "tick_data.dat";
+    std::ifstream input_data_infile;
+    std::ofstream tick_data_outfile;
+
+    int milliseconds_in_day = 24*60*60*1000, date;
+    double fed_rate_old, fed_rate_new, tick_value, drift_per_tick;
+
+    // Setup the standard normal generator
+    srand(time(NULL));
+    std::default_random_engine generator;
+    std::normal_distribution<double> standard_normal(0,1);
+
+    // Get (and print) the program paramters.
+    Parameters params(parameters_filename);
+    params.print();
+
+    // Determine the number of ticks per day and the standard deviation of each tick.
+    int ticks_per_day = milliseconds_in_day/params.time_bet_ticks - 1;
+    double stdev_per_tick = params.standard_error/std::sqrt(ticks_per_day);
+
+    // Open the input and output files
+    input_data_infile.open(input_data_filename);
+    if (!input_data_infile.is_open()){
+        std::cout << "ERROR: input_data.txt file could not be opened. Exiting.\n";
+        exit(1);
+    }
+    tick_data_outfile.open(tick_data_filename);
+    if (!tick_data_outfile.is_open()){
+        std::cout << "ERROR: tick_data.dat file could not be opened. Exiting.\n";
+        exit(1);
+    }
+
+    // Get the initial fed fund rate
+    input_data_infile >> date;
+    input_data_infile >> fed_rate_old;
+    tick_data_outfile << fed_rate_old << "\n";
+
+    // For each daily fed fund rate, create the appropriate number of intraday ticks
+    while (!input_data_infile.eof()){
+        input_data_infile >> date;
+        input_data_infile >> fed_rate_new;
+        drift_per_tick = (fed_rate_new - fed_rate_old)/ticks_per_day;
+        tick_value = fed_rate_old;
+        for (int j=0; j<ticks_per_day; ++j){
+            tick_value += drift_per_tick + stdev_per_tick*standard_normal(generator);
+            tick_data_outfile << tick_value << "\n";
+        }
+        tick_data_outfile << fed_rate_new << "\n";
+        fed_rate_old = fed_rate_new;
+    }
+
+    input_data_infile.close();
+    tick_data_outfile.close();
+    std::cout << "\n";
+    return 0;
+}
---
+++ AssignmentC/parameters.h
@@ -0,0 +1,51 @@
+#ifndef PARAMETERS_INCLUDED
+#define PARAMETERS_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct Parameters{
+
+    int time_bet_ticks;
+    float standard_error;
+
+    Parameters(const char* parameters_filename); // Constructor
+    const char* get_param(std::ifstream &infile);
+    void print();
+};
+
+// Constructor. Read all the paramaters from the input files.
+Parameters::Parameters(const char* parameters_filename)
+{
+    std::ifstream parameters_infile;
+    parameters_infile.open(parameters_filename);
+    if (!parameters_infile.is_open()){
+        std::cout << "ERROR: parameters.txt file could not be opened. Exiting.\n";
+        exit(1);
+    }
+
+    time_bet_ticks = atoi(get_param(parameters_infile));
+    standard_error = atof(get_param(parameters_infile));
+
+    parameters_infile.close();
+}
+
+// Get a single parameter
+const char* Parameters::get_param(std::ifstream &infile)
+{
+    std::string text;
+    getline(infile, text, ','); // data is always right after a comma
+    getline(infile, text);
+    return text.c_str();
+}
+
+// Print all the parameters and initial state
+void Parameters::print()
+{
+    std::cout << "\nParameters"
+              << "\nAverage time between ticks in milliseconds: " << time_bet_ticks
+              << "\nStandard error of original data: " << standard_error << "\n\n";
+}
+
+#endif // PARAMETERS_INCLUDED
