5d165c6005a5f0b445d0954b6c001f3749b6c473
sampfeiffer <sampfeiffer54@gmail.com>
2015-03-22 11:18:30 -0700

    test_hadoop.cpp working

--- AssignmentC/test_hadoop.cpp
+++ AssignmentC/test_hadoop.cpp
@@ -1,60 +1,61 @@
 #include <algorithm>
 #include <limits>
 #include <string>
- 
+#include <sstream>
+#include <stdlib.h>
+
 #include  "stdint.h"  // <--- to prevent uint64_t errors!
- 
+
 #include "hadoop/Pipes.hh"
 #include "hadoop/TemplateFactory.hh"
 #include "hadoop/StringUtils.hh"
- 
+
 using namespace std;
 
 class WordCountMapper : public HadoopPipes::Mapper {
 public:
-  // constructor: does nothing
-  WordCountMapper( HadoopPipes::TaskContext& context ) {
-  }
+    // constructor: does nothing
+    WordCountMapper( HadoopPipes::TaskContext& context ) {}
 
-  // map function: receives a line, outputs (word,"1")
-  // to reducer.
-  void map( HadoopPipes::MapContext& context ) {
-    //--- get line of text ---
-    string line = context.getInputValue();
-
-    //--- split it into words ---
-    vector< string > words =
-      HadoopUtils::splitString( line, " " );
-
-    //--- emit each word tuple (word, "1" ) ---
-    for ( unsigned int i=0; i < words.size(); i++ ) {
-      context.emit( words[i], HadoopUtils::toString( 1 ) );
+    // map function: receives a line, outputs (word,"1")
+    // to reducer.
+    void map( HadoopPipes::MapContext& context )
+    {
+        //--- get line of text ---
+        std::string value = context.getInputValue();
+        float float_value = atof(value.c_str());
+        if (float_value < 10){
+            context.emit( "sum", value );
+            std::stringstream temp;
+            temp << float_value*float_value;
+            context.emit( "sum_squared", temp.str());
+        }
     }
-  }
 };
- 
+
 class WordCountReducer : public HadoopPipes::Reducer {
 public:
-  // constructor: does nothing
-  WordCountReducer(HadoopPipes::TaskContext& context) {
-  }
+    // constructor: does nothing
+    WordCountReducer(HadoopPipes::TaskContext& context) {}
 
-  // reduce function
-  void reduce( HadoopPipes::ReduceContext& context ) {
-    int count = 0;
+    // reduce function
+    void reduce( HadoopPipes::ReduceContext& context )
+    {
+        float count = 0;
 
-    //--- get all tuples with the same key, and count their numbers ---
-    while ( context.nextValue() ) {
-      count += HadoopUtils::toInt( context.getInputValue() );
+        //--- get all tuples with the same key, and count their numbers ---
+        while ( context.nextValue() ) {
+          count += HadoopUtils::toFloat( context.getInputValue() );
+        }
+
+        //--- emit (word, count) ---
+        context.emit(context.getInputKey(), HadoopUtils::toString( count ));
     }
-
-    //--- emit (word, count) ---
-    context.emit(context.getInputKey(), HadoopUtils::toString( count ));
-  }
 };
- 
-int main(int argc, char *argv[]) {
-  return HadoopPipes::runTask(HadoopPipes::TemplateFactory<
-                              WordCountMapper,
-                              WordCountReducer >() );
+
+int main(int argc, char *argv[])
+{
+      return HadoopPipes::runTask(HadoopPipes::TemplateFactory<
+                                  WordCountMapper,
+                                  WordCountReducer >() );
 }
--- AssignmentC/wordcount.cpp
+++ AssignmentC/wordcount.cpp
@@ -1,13 +1,14 @@
 #include <algorithm>
 #include <limits>
 #include <string>
- 
+#include <stdlib.h>
+
 #include  "stdint.h"  // <--- to prevent uint64_t errors!
- 
+
 #include "hadoop/Pipes.hh"
 #include "hadoop/TemplateFactory.hh"
 #include "hadoop/StringUtils.hh"
- 
+
 using namespace std;
 
 class WordCountMapper : public HadoopPipes::Mapper {
@@ -21,18 +22,13 @@ public:
   void map( HadoopPipes::MapContext& context ) {
     //--- get line of text ---
     string line = context.getInputValue();
-
-    //--- split it into words ---
-    vector< string > words =
-      HadoopUtils::splitString( line, " " );
+    float test = atoi(line.c_str());
 
     //--- emit each word tuple (word, "1" ) ---
-    for ( unsigned int i=0; i < words.size(); i++ ) {
-      context.emit( words[i], HadoopUtils::toString( 1 ) );
-    }
+    context.emit( line, HadoopUtils::toString(test) );
   }
 };
- 
+
 class WordCountReducer : public HadoopPipes::Reducer {
 public:
   // constructor: does nothing
@@ -52,7 +48,7 @@ public:
     context.emit(context.getInputKey(), HadoopUtils::toString( count ));
   }
 };
- 
+
 int main(int argc, char *argv[]) {
   return HadoopPipes::runTask(HadoopPipes::TemplateFactory<
                               WordCountMapper,
